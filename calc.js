4c98bb672f8da46a8636ffa15964eab495623db8
Andrzej Dawidziuk <andrzej.dawidziuk@gmail.com>
2019-01-31 21:28:28 +0100

    Add arithmetic operations

--- calc.js
+++ calc.js
@@ -1,22 +1,80 @@
-function addNumericButtons() {
+let display = document.getElementById('display-field');
+let memory_value;
+let previous_value;
+let calc_operation;
+let clear_display_flag;
 
-    function addButton(innerText) {
-        let buttonsDiv = document.getElementById('main-button-bar');
-        let button = document.createElement("BUTTON");
-        buttonsDiv.appendChild(button);
-        button.className = "button";
-        button.innerText = innerText;
-        button.addEventListener('click', (el) => {alert(innerText)});
-    }
-
-    let i;
-    for (i = 1; i <= 9; ++i) {
-        addButton(i)
-    }
-    addButton(0)
-    addButton('.')
-
+function init() {
+    memory_value = 0;
+    previous_value = 0;
+    display.value = '0';
+    calc_operation = (x, y) => {return y};
+    clear_display_flag = false;
 }
 
-addNumericButtons()
+function updateDisplay(val) {
+    if (clear_display_flag) {
+        display.value = '0';
+        clear_display_flag = false;
+    }
+
+    switch (val) {
+        case '.':
+            if (display.value.indexOf('.') === -1) {
+                display.value = display.value + '.';
+            }
+            break;
+        default:
+            if (display.value === '0') {
+                display.value = val;
+            } else {
+                display.value = display.value + val
+            }
+    }
+}
+
+function set_operation(op) {
+    previous_value = parseFloat(display.value);
+    clear_display_flag = true;
+    calc_operation = op;
+}
+
+function execute() {
+    display.value = calc_operation(previous_value, parseFloat(display.value));
+    clear_display_flag = true;
+}
+
+function addButton(innerText) {
+    let buttonsDiv = document.getElementById('main-button-bar');
+    let button = document.createElement("BUTTON");
+    buttonsDiv.appendChild(button);
+    button.className = "button";
+    button.innerText = innerText;
+    button.addEventListener('click', () => {updateDisplay(innerText);});
+}
+
+
+for (let i = 1; i <= 9; ++i) addButton(i);
+addButton(0);
+addButton('.');
+
+document.getElementById('btn-ac')
+    .addEventListener('click', () => {init()});
+
+document.getElementById('btn-add').addEventListener("click", () => {
+    set_operation((x, y) => {return x + y})
+});
+document.getElementById('btn-sub').addEventListener("click", () => {
+    set_operation((x, y) => {return x - y})
+});
+document.getElementById('btn-mul').addEventListener("click", () => {
+    set_operation((x, y) => {return x * y})
+});
+document.getElementById('btn-div').addEventListener("click", () => {
+    set_operation((x, y) => {return x / y})
+});
+
+document.getElementById("btn-exe").addEventListener("click", execute);
+
+init();
 
